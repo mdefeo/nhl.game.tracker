ba5646ee350e0a51d7d67fd507e7a1f308bc3502
Marcello De Feo <marcellodefeo@outlook.com>
2024-04-09 21:06:48 -0400

    Landing page

--- app/components/utilities/Skeleton.tsx
+++ app/components/utilities/Skeleton.tsx
@@ -2,10 +2,10 @@
 const Skeleton: React.FC = () => {
   return( 
     <div className="flex flex-col gap-4 w-full">
-      <div className="skeleton h-8 w-full"></div>
-      <div className="skeleton h-8 w-full"></div>
-      <div className="skeleton h-8 w-full"></div>
-      <div className="skeleton h-8 w-full"></div>
+      <div className="skeleton h-16 w-full"></div>
+      <div className="skeleton h-16 w-full"></div>
+      <div className="skeleton h-16 w-full"></div>
+      <div className="skeleton h-16 w-full"></div>
     </div>
   );
 };
